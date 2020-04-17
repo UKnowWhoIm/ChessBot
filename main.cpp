7d4b116c283accd791cdd9f80bfb89ad7cc2b462
UKnowWhoIm <51323747+UKnowWhoIm@users.noreply.github.com>
2020-04-18 04:54:46 +0530

    Added Initial TargetArea

--- main.cpp
+++ main.cpp
@@ -1,40 +1,20 @@
-#include <bitset>
-#include<stdio.h>
-#include<iostream>
-#include<map>
-#include<ctime>
-#include<iterator>
+#include "game.h"
+
 using namespace std;
 
-struct Comparer {
-    bool operator() (const bitset<64> &b1, const bitset<64> &b2) const {
-        return b1.to_ulong() < b2.to_ulong();
+void disp_board(bitset<64> board){
+    for(int i=0;i<64;i++){
+        if(i % 8 == 0)
+            cout<<'\n';
+        cout<<board[63-i];
+
     }
-};
-
-
-void BubbleSort(int arr[100], int n){
-    int temp;
-    for(int i=0,j=0;i<n;i++)
-        for(j=i;j<n;j++)
-            if(arr[j] < arr[i]){
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
 }
-
 
 int main()
 {
-    const long N = 10000/2;
-    int arr[N];
-    for(long i=N; i > 0; i--)
-        arr[N-i] = i;
+    game GameObj = game();
+    char board[65] = "rnbqkbnrppppppppffffffffffffffffffffffffffffffffPPPPPPPPRNBQKBNR";
 
-    //cout<<arr[0]<<' '<<arr[N-1];
-    time_t a = time(NULL);
-    BubbleSort(arr, N);
-    cout << time(NULL) - a;
     return 0;
 }
--- src/game.cpp
+++ src/game.cpp
@@ -15,6 +15,187 @@ game::game(char board[64])
 
 }
 */
+bool return_true(int i){
+    // Since NULL is just 0,  this is a hotfix for Updation arr initialization
+    return true;
+}
+
+struct Updation{
+    int updation;
+    int times;
+};
+
+int* get_true_pos(bitset<64> board){
+    // Find the true values in a bitboard
+    int *posns = (int*)malloc(sizeof(int)*17);
+    int k=0;
+    for(int i=0;i<64;i++){
+        if(board[63-i])
+            posns[k++] = i;
+    }
+    // Set last element to -1 to indicate arr has ended
+    posns[k] = -1;
+    return posns;
+}
+
+string get_player(char piece){
+    if(piece == 'f')
+            return "";
+    if(piece > 96)
+        return BLACK;
+    return WHITE;
+}
+
+bool can_go_left(int pos){
+    return pos % 8 != 0;
+}
+
+bool can_go_right(int pos){
+    return pos % 8 != 7;
+}
+
+int how_many_times_can_go_left(int pos){
+    return pos % 8;
+}
+
+int how_many_times_can_go_right(int pos){
+    return 7 - pos % 8;
+}
+
+
+string reverse_player(string player){
+    if(player == WHITE)
+        return BLACK;
+    return WHITE;
+}
+
+bitset<64> move_generator(char board[65], int pos, Updation updation[4], int valid_updations){
+    int pos_temp;
+    bitset<64> area (0);
+    for(int i=0, k=0; i < valid_updations; i++){
+        pos_temp = pos + updation[i].updation;
+        for(k=0;pos_temp >= 0 && pos_temp <= 63; pos_temp += updation[i].updation, k++){
+            if(updation[i].times == k)
+                break;
+            area[63-pos_temp] = 1;
+            if(board[pos_temp] != 'f')
+                // Current col is occupied
+                break;
+        }
+    }
+    return area;
+}
+
+
+
+int find_king(char board[65], string player){
+    char target;
+    if(player == WHITE)
+        target = 'K';
+    else
+        target = 'k';
+    for(int i=0;i<64;i++)
+        if(board[i] == target)
+            return i;
+}
+
+bitset<64> get_valid_moves(char board[65], int pos, string player){
+    bitset<64> area (0);
+    if(tolower(board[pos]) == 'p'){
+        int dirn;
+        bool initial_move;
+        if(player == WHITE){
+            dirn = -1;
+            initial_move = pos <= 55 && pos >= 48;
+        }
+        else{
+            dirn = 1;
+            initial_move = pos <= 15 && pos >= 8;
+        }
+        area[63 - (pos + 8*dirn)] = 1;
+        if(board[pos + 8*dirn] == 'f' && initial_move)
+            // Initial Double Step
+            area[63 - (pos + 16*dirn)] = 1;
+        if(can_go_left(pos) && get_player(board[pos + 8*dirn - 1]) == reverse_player(player))
+            // Left Side Capture
+            area[63 - (pos + 8*dirn - 1)] = 1;
+        if(can_go_right(pos) && get_player(board[pos + 8*dirn + 1]) == reverse_player(player))
+            // Right Side Capture
+            area[63 - (pos + 8*dirn + 1)] = 1;
+        // ENPASSANT GOES HERE
+
+    }
+    else if(tolower(board[pos]) == 'n'){
+        int posns[8],k=0;
+        if(can_go_left(pos)){
+            posns[k++] = -17; // up
+            posns[k++] = 15; // down
+            if(can_go_left(pos - 1)){
+                posns[k++] = 6; // down
+                posns[k++] = -10; // up
+            }
+        }
+        if(can_go_right(pos)){
+            posns[k++] = -15; // up
+            posns[k++] = 17; // down
+            if(can_go_right(pos + 1)){
+                posns[k++] = -6; // up
+                posns[k++] = 10; // down
+            }
+        }
+        for(int i=0; i < k; i++)
+            if( pos + posns[i] >= 0 && pos + posns[i] < 64)
+                area[63 - (pos + posns[i])] = 1;
+    }
+    else if(tolower(board[pos]) == 'k'){
+        int posns[10] = {-8, 8, 0, 0, 0, 0, 0, 0, 0, 0}, k = 2;
+        if(can_go_left(pos)){
+            posns[k++] = -1;
+            posns[k++] = -9;
+            posns[k++] = 7;
+        }
+        if(can_go_right(pos)){
+            posns[k++] = 1;
+            posns[k++] = -7;
+            posns[k++] = 9;
+        }
+        for(int i=0; i < k; i++)
+            if( pos + posns[i] >= 0 && pos + posns[i] < 64 )
+                area[63 - (pos + posns[i])] = 1;
+    }
+    else{
+        // Q, B, R
+        Updation rook_updation[4];
+        Updation bishop_updation[4];
+        rook_updation[0] = {-8, 7};
+        rook_updation[1] = {8, 7};
+        int r = 2, b = 0;
+        // how_many_times_can_go_left and how_many_times_can_go_right returns the maximum
+        // number of times the piece can go in that particular dirn if the board is empty
+        // Since it isn't constrained to vertical bounds it may not always be true
+        if(can_go_left(pos)){
+            rook_updation[r++] = {-1, how_many_times_can_go_left(pos)};
+            bishop_updation[b++] = {-9, how_many_times_can_go_left(pos)};
+            bishop_updation[b++] = {7, how_many_times_can_go_left(pos)};
+        }
+        if(can_go_right(pos)){
+           rook_updation[r++] = {1, how_many_times_can_go_right(pos)};
+            bishop_updation[b++] = {-7, how_many_times_can_go_right(pos)};
+            bishop_updation[b++] = {9, how_many_times_can_go_right(pos)};
+        }
+        if(tolower(board[pos]) == 'b')
+            return move_generator(board, pos, bishop_updation, b);
+        else if(tolower(board[pos]) == 'r')
+            return move_generator(board, pos, rook_updation, r);
+        else{
+            // Queen = Rook + Bishop
+            area = move_generator(board, pos, bishop_updation, b);
+            area |= move_generator(board, pos, rook_updation, r);
+        }
+    }
+    return area;
+}
+
 
 game::game(){
     //constructor
@@ -22,10 +203,19 @@ game::game(){
     strcpy(this->game_board, board);
     for(int i=0; i < 64; i++){
         if(this->game_board[i] != 'f' && get_player(this->game_board[i]) == WHITE)
-            white_occupied[64-i] = 1;
+            white_occupied[63-i] = 1;
         else if(this->game_board[i] != 'f' && get_player(this->game_board[i]) == BLACK)
-            black_occupied[64-i] = 1;
+            black_occupied[63-i] = 1;
     }
+    int *white_pieces = get_true_pos(white_occupied);
+    int *black_pieces = get_true_pos(black_occupied);
+    int *ptr;
+    for(ptr = white_pieces; *ptr != -1; ptr++){
+        cout<<*ptr<<'\n';
+        this->target_area[*ptr] = get_valid_moves(this->game_board, *ptr, WHITE);
+    }
+    for(ptr = black_pieces; *ptr != -1; ptr++)
+        this->target_area[*ptr] = get_valid_moves(this->game_board, *ptr, BLACK);
 }
 
 bitset<64> game::get_occupied(string player){
@@ -117,3 +307,4 @@ game::~game()
 {
     //destructor
 }
+
